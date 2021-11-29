8732cf006d63700655d4771b28982b9bb12e77b5
Dan Njoku <dnjoku@ups.com>
2021-11-29 18:20:41 -0500

    Load extensions: fixed typo

--- ssg/extensions.py
+++ ssg/extensions.py
@@ -4,7 +4,7 @@ import importlib
 from pathlib import Path
 
 def load_module(directory, name):
-    sys.path.isert(0, directory)
+    sys.path.insert(0, directory)
     importlib.import_module(name)
     sys.path.pop(0)
 
